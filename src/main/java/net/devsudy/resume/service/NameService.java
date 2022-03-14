77a93ba3f399468dc58e04900e52aa1e0a8aa1e8
drdima2 <drdima2@gmail.com>
2022-03-14 15:13:36 -0500

    first commit

---
+++ src/main/java/net/devsudy/resume/service/NameService.java
@@ -0,0 +1,11 @@
+package net.devsudy.resume.service;
+
+public class NameService {
+    public static NameService getInstance(){
+        return new NameService();
+    }
+
+    public String convertName(String name){
+        return name.toUpperCase();
+    }
+}
